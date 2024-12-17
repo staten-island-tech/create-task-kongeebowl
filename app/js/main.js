cc4c310c936035314b73027756a39a1b0b746621
kongeebowl <kadenp11@nycstudents.net>
2024-12-17 12:31:32 -0500

    aweaweaweaewae

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,8 @@ function colorCards() {
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="card w-[90%] sm:w-4/5 md:w-2/5 lg:w-1/5 h-auto  bg-[#150709] rounded-3xl flex flex-col items-center justify-around m-8 border-2 border-[#e0d1ad]">
-            <h2 class="card-title text-2xl text-[#e0d1ad]">${color.name}</h2>
+      <div class="card w-[90%] sm:w-25 md:w-25 lg:w-21 h-auto  rounded-3xl flex flex-col items-center justify-around m-8 border-2 bg-primary">
+            <h2 class="card-title text-2xl ">${color.name}</h2>
             <img src="${color.image}" alt="${color.name}, ${color.rarity}" class="object-contain w-[65%]" />
           </div>
       `
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./js/main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["light", "dark", "luxury", "nord"],
+  },
   theme: {
     extend: {
-   
+      colors: { whiteish: "var(--whiteish)" },
     },
   },
   plugins: [require("daisyui")],
