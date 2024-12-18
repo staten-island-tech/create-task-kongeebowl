4ce5c4cb2cd34911422f1bd529751da47f44df8f
kongeebowl <kadenp11@nycstudents.net>
2024-12-18 12:30:16 -0500

    q23

--- app/js/colors.js
+++ app/js/colors.js
@@ -1,14 +1,60 @@
 const colors = [
-  { name: "Red", rarity: "Common", image: "a" },
-  { name: "Blue", rarity: "Uncommon", image: "a" },
-  { name: "Green", rarity: "Epic", image: "a" },
-  { name: "Yellow", rarity: "Common", image: "a" },
-  { name: "Purple", rarity: "Legendary", image: "a" },
-  { name: "Orange", rarity: "Uncommon", image: "a" },
-  { name: "Pink", rarity: "Epic", image: "a" },
-  { name: "Cyan", rarity: "Common", image: "a" },
-  { name: "Black", rarity: "Legendary", image: "a" },
-  { name: "White", rarity: "Uncommon", image: "a" },
+  {
+    name: "Red",
+    rarity: "Common",
+    image: "https://www.colorcombos.com/images/colors/FF0000.png",
+  },
+  {
+    name: "Yellow",
+    rarity: "Common",
+    image: "https://www.colorcombos.com/images/colors/FFCC00.png",
+  },
+
+  {
+    name: "Blue",
+    rarity: "Uncommon",
+    image: "https://www.colorcombos.com/images/colors/000084.png",
+  },
+  {
+    name: "White",
+    rarity: "Uncommon",
+    image: "https://www.colorcombos.com/images/colors/FFFFFF.png",
+  },
+
+  {
+    name: "Green",
+    rarity: "Epic",
+    image: "https://www.colorcombos.com/images/colors/55D43F.png",
+  },
+  {
+    name: "Pink",
+    rarity: "Epic",
+    image: "https://www.colorcombos.com/images/colors/FFBAD2.png",
+  },
+
+  {
+    name: "Black",
+    rarity: "Legendary",
+    image: "https://www.colorcombos.com/images/colors/000000.png",
+  },
+  {
+    name: "Purple",
+    rarity: "Legendary",
+    image: "https://www.colorcombos.com/images/colors/B378D3.png",
+  },
+
+  {
+    name: "Rainbow",
+    rarity: "Mythic",
+    image:
+      "https://static.vecteezy.com/system/resources/thumbnails/006/922/783/small_2x/rainbow-spectrum-of-seven-smoothly-changing-colors-colorful-background-vector.jpg",
+  },
+  {
+    name: "Cool Gradient",
+    rarity: "Godly",
+    image:
+      "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1815&fit=crop&crop=faces",
+  },
 ];
 
 export { colors };
--- app/js/main.js
+++ app/js/main.js
@@ -9,9 +9,10 @@ function colorCards() {
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="card w-[90%] sm:w-25 md:w-25 lg:w-21 h-auto  rounded-3xl flex flex-col items-center justify-around m-8 border-2 bg-primary">
+      <div class="card w-[90%] sm:w-[50%] md:w-[33%] lg:w-[15%] h-auto  rounded-3xl flex flex-col items-center justify-around m-4 border-2 bg-primary">
             <h2 class="card-title text-2xl ">${color.name}</h2>
-            <img src="${color.image}" alt="${color.name}, ${color.rarity}" class="object-contain w-[65%]" />
+            <img src="${color.image}" alt="${color.name}, ${color.rarity}" class="object-contain w-[65%] " />
+            <p class="text-xl">${color.rarity}</p>
           </div>
       `
     );
