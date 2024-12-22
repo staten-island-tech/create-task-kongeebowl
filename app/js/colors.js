887268e886c3be8481192adee03768076c6ba682
kongeebowl1 <kadenpang09@gmail.com>
2024-12-22 18:56:05 -0500

    i love you chatgpty

--- app/js/colors.js
+++ app/js/colors.js
@@ -3,44 +3,52 @@ const colors = [
     name: "Red",
     rarity: "Common",
     image: "https://www.colorcombos.com/images/colors/FF0000.png",
+    weight: 50,
   },
   {
     name: "Yellow",
     rarity: "Common",
     image: "https://www.colorcombos.com/images/colors/FFCC00.png",
+    weight: 50,
   },
 
   {
     name: "Blue",
     rarity: "Uncommon",
     image: "https://www.colorcombos.com/images/colors/000084.png",
+    weight: 35,
   },
   {
     name: "White",
     rarity: "Uncommon",
     image: "https://www.colorcombos.com/images/colors/FFFFFF.png",
+    weight: 35,
   },
 
   {
     name: "Green",
     rarity: "Epic",
     image: "https://www.colorcombos.com/images/colors/55D43F.png",
+    weight: 10,
   },
   {
     name: "Pink",
     rarity: "Epic",
     image: "https://www.colorcombos.com/images/colors/FFBAD2.png",
+    weight: 10,
   },
 
   {
     name: "Black",
     rarity: "Legendary",
     image: "https://www.colorcombos.com/images/colors/000000.png",
+    weight: 5,
   },
   {
     name: "Purple",
     rarity: "Legendary",
     image: "https://www.colorcombos.com/images/colors/B378D3.png",
+    weight: 5,
   },
 
   {
@@ -48,12 +56,14 @@ const colors = [
     rarity: "Mythic",
     image:
       "https://static.vecteezy.com/system/resources/thumbnails/006/922/783/small_2x/rainbow-spectrum-of-seven-smoothly-changing-colors-colorful-background-vector.jpg",
+    weight: 1,
   },
   {
     name: "Cool Gradient",
     rarity: "Godly",
     image:
       "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1815&fit=crop&crop=faces",
+    weight: 0.1,
   },
 ];
 
--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,17 @@ import { colors } from "./colors";
 console.log(colors);
 
 let money = 500;
-gamble(money);
+gambleButton(money);
 
 function colorCards() {
   colors.forEach((color) => {
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="card w-[90%] sm:w-[50%] md:w-[33%] lg:w-[15%] h-auto  rounded-3xl flex flex-col items-center justify-around m-4 border-2 bg-primary">
-            <h2 class="card-title text-2xl ">${color.name}</h2>
+      <div class="card w-[90%] sm:w-[50%] md:w-[33%] lg:w-[15%] h-auto  rounded-3xl flex flex-col items-center justify-around m-4 bg-neutral">
+            <h2 class="card-title text-2xl text-accent">${color.name}</h2>
             <img src="${color.image}" alt="${color.name}, ${color.rarity}" class="object-contain w-[65%] " />
-            <p class="text-xl">${color.rarity}</p>
+            <p class="text-xl text-accent">${color.rarity}</p>
           </div>
       `
     );
@@ -23,11 +23,28 @@ function colorCards() {
 }
 colorCards();
 
+function gachaPull() {
+  // Calculate the total weight
+  const totalWeight = colors.reduce((sum, color) => sum + color.weight, 0);
+
+  // Generate a random number between 0 and totalWeight
+  const randomNum = Math.random() * totalWeight;
+
+  // Determine which color is selected based on the random number
+  let cumulativeWeight = 0;
+  for (const color of colors) {
+    cumulativeWeight += color.weight;
+    if (randomNum <= cumulativeWeight) {
+      return color;
+    }
+  }
+}
+
 function clearText() {
   DOMSelectors.money.innerHTML = "";
 }
 
-function gamble(money) {
+function gambleButton(money) {
   DOMSelectors.money_button.addEventListener("click", () => {
     if (money >= 10) {
       money -= 10;
@@ -37,8 +54,14 @@ function gamble(money) {
         "afterbegin",
         `<p class="text-xl">Money: $${money}</p>`
       );
+      gachaPull();
+      const pulledColor = gachaPull();
+      console.log(`You got: ${pulledColor.name} (${pulledColor.rarity})`);
+      console.log(`Image: ${pulledColor.image}`);
     } else {
       console.log("you broke as hell");
     }
   });
 }
+
+function gachaHistory() {}
